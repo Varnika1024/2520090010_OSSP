23b895e0a7bfbc773c05d169d4cfcfae38bd3e74
VARNIKA KOMALI <varnikakomali@VARNIKAs-MacBook-Air.local>
2026-08-11 22:10:24 +0530

    Step 4: Implement Makefile and build process

---
+++ 2520090010_SKILL/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("======================================\n");
+    printf(" Shell Architecture & Makefile Demo   \n");
+    printf(" Flow: User -> Shell -> Kernel -> Hardware\n");
+    printf("======================================\n");
+    return 0;
+}
