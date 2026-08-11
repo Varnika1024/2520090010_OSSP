51a8853e5a785525cb3e194662248b22159131d2
Varnika1024 <komalivarnika3011@gmail.com>
2026-08-11 22:19:49 +0530

    Delete 2520090010_SKILL directory

--- 2520090010_SKILL/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("======================================\n");
-    printf(" Shell Architecture & Makefile Demo   \n");
-    printf(" Flow: User -> Shell -> Kernel -> Hardware\n");
-    printf("======================================\n");
-    return 0;
-}
